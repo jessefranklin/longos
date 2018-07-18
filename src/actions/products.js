ebdabfefe189585535a245705c5d2d190b3e744d
jessefranklin <jessefrankling@gmail.com>
2018-07-18 09:40:17 -0400

    Products fetch api call with header config (Commented out)

--- src/actions/products.js
+++ src/actions/products.js
@@ -6,12 +6,35 @@ export function fetchProducts() {
     };
 }
   
-function handleErrors(response) {
-    if (!response.ok) {
-        throw Error(response.statusText);
+
+/* let axios = require('axios');
+
+const url = "http://digitalpreorder.azurewebsites.net/api/product";
+let url2 = "https://api.github.com/search/users?q=language:javascript+location:lagos&sort=repositories&order=desc"
+const store = "?store=store1"
+const headers = {
+    dataType: 'json',
+    mode: 'no-cors',
+    header: {
+        "Access-Control-Allow-Origin": "*"
     }
-    return response;
 }
+
+export let fetchProducts = () => {
+    return (dispatch) => {
+        dispatch(fetchProductsBegin())
+        return axios.get(url, headers).then(
+            (response) => {
+                console.log(response.data);
+            },
+            (err) => {
+                console.log(err);
+            }
+        )
+
+    }
+}
+  */
 
 export const FETCH_PRODUCTS_BEGIN   = 'FETCH_PRODUCTS_BEGIN';
 export const FETCH_PRODUCTS_SUCCESS = 'FETCH_PRODUCTS_SUCCESS';
