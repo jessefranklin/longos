d5c02ec81f7576b2d483b260301daa9953089ba3
jessefranklin <jessefrankling@gmail.com>
2018-09-06 11:13:10 -0400

    Ability to search by product number

--- src/selectors/products.js
+++ src/selectors/products.js
@@ -1,6 +1,10 @@
 export default (products, {text}) => {
   return products.filter((product) => {
-    const textMatch = product.description.toLowerCase().includes(text.toLowerCase()) || product.name.toLowerCase().includes(text.toLowerCase()) || product.category.toLowerCase().includes(text.toLowerCase());
+    const textMatch = product.description.toLowerCase().includes(text.toLowerCase()) 
+    || product.name.toLowerCase().includes(text.toLowerCase()) 
+    || product.category.toLowerCase().includes(text.toLowerCase())
+    || product.productNumber.toLowerCase().includes(text.toLowerCase())
+    || product.id.toLowerCase().includes(text.toLowerCase());
     
     return textMatch;
   });
