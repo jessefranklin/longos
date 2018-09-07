b9560f2fc3fe9f9763dc8aa038d0eb649cd8dbf2
James Kirkwood <kirkwood.jamie@gmail.com>
2018-09-07 15:23:11 -0400

    second style review with tony

--- src/components/cart/CartOrderConfirmation.js
+++ src/components/cart/CartOrderConfirmation.js
@@ -23,6 +23,7 @@ class CartOrderConfirmation extends React.Component {
           <p className="confirm-text">Thanks for your order.</p>
           <p>Order #<strong>{this.props.order.orderId}</strong> has been placed</p>
           <p>We'll see you on <strong>{moment(this.props.order.pickUpDate).format('MMMM Do, YYYY')}</strong> at <strong>{moment().startOf('day').seconds(this.props.order.time).format('h:mm A')}</strong></p>
+          <p className="confirm-note">Note: If you decide to edit or cancel your order please call the store at: <strong>{this.props.settings.store.phoneNo}</strong></p>
         </div>
         <CustomerFeedback />
       </div>
@@ -35,7 +36,8 @@ const mapStateToProps = (state) => ({
     profile: state.profile,
     cart: state.cart,
     cartTotal: cartTotal(state.cart),
-    order: state.order
+    order: state.order,
+    settings: state.settings
 });
 
 
